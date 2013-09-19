8eda7d02c1dd93f5c2be00d794e7713b76e49fd4
numa08 <n511287@gmail.com>
2013-09-19 10:56:55 +0000

    ソースコードを追加

--- apps/AmazonListApp/src/net/numa08/amazonlistapp/MainActivity.scala
+++ apps/AmazonListApp/src/net/numa08/amazonlistapp/MainActivity.scala
@@ -1,7 +1,13 @@
 package net.numa08.amazonlistapp
 
 import android.app.Activity
+import android.widget.TextView
+import android.os.Bundle
 
 class MainActivity extends Activity {
 
+  override def onCreate(savedInstanceState:Bundle) : Unit = {
+    super.onCreate(savedInstanceState)
+    Option(new TextView(this)).foreach(t => {t.setText("hello scala"); setContentView(t)})
+  }
 }
